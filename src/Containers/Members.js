5324f61255f057fef0ba58fcd3d6fad91088c4ea
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2021-12-31 01:38:41 +0100

    member & trip detail modal

--- src/Containers/Members.js
+++ src/Containers/Members.js
@@ -1,16 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { List, Avatar, Input, Button, Tooltip } from 'antd';
+import { List, Avatar, Input, Button, Tooltip, Modal } from 'antd';
 import { loadMembers } from '../api';
 import { ExpandAltOutlined } from '@ant-design/icons';
+import moment from 'moment';
 
 const { Search } = Input;
 
 export default () => {
     const [members, setMembers] = useState([]);
     const [membersForDisplay, setMembersForDisplay] = useState([]);
+    const [showDetailModal, setShowDetailModal] = useState(false);
+    const [detailIndex, setDetailIndex] = useState(-1);
+
     useEffect(() => {
         fetchData();
     }, []);
+
+    function closeModal() {
+        setShowDetailModal(false);
+        setDetailIndex(-1);
+    }
+
+    function detailModal() {
+        if (detailIndex == -1) return <></>;
+        const member = membersForDisplay[detailIndex];
+        return (
+            <Modal title="Member details" visible={showDetailModal} onOk={closeModal} onCancel={closeModal} width={'70vw'}>
+                <p>Full name: <b>{member.fullName}</b></p>
+                <p>Email: <b>{member.email}</b></p>
+                <p>Birthdate: <b>{moment(member.birthdate).format("DD.MM.yyyy")}</b></p>
+                <p>Joined: <b>{moment(member.createdAt).format("DD.MM.yyyy")}</b></p>
+
+                <List
+                    itemLayout="horizontal"
+                    dataSource={member.trips}
+                    renderItem={trip => (
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={<Avatar src="https://cdn-icons-png.flaticon.com/512/1452/1452378.png" />}
+                                title={trip.tripName}
+                                description={`${trip.destination} - ${moment(trip.tripDate, "YYYYMMDD").fromNow()}`}
+                            />
+                        </List.Item>
+                    )}
+                />
+            </Modal>
+        )
+    }
+
+    function openDetailmodal(idx) {
+        setDetailIndex(idx);
+        setShowDetailModal(true);
+    }
 
     async function fetchData() {
         const members = await loadMembers();
@@ -28,10 +69,10 @@ export default () => {
             <List
                 itemLayout="horizontal"
                 dataSource={membersForDisplay}
-                renderItem={member => (
+                renderItem={(member, idx) => (
                     <List.Item
                         actions={[<Tooltip title="View detail">
-                            <Button type="primary" shape="circle" icon={<ExpandAltOutlined />} size="large" />
+                            <Button type="primary" shape="circle" onClick={() => openDetailmodal(idx)} icon={<ExpandAltOutlined />} size="large" />
                         </Tooltip>]}
                     >
                         <List.Item.Meta
@@ -42,6 +83,7 @@ export default () => {
                     </List.Item>
                 )}
             />
+            {detailModal()}
         </div>
     )
 }
--- src/Containers/Trips.js
+++ src/Containers/Trips.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
-import { List, Avatar, Input, Button, Tooltip, Switch } from 'antd';
+import { List, Avatar, Input, Button, Tooltip, Switch, Modal } from 'antd';
 import { loadTrips } from '../api';
 import { ExpandAltOutlined } from '@ant-design/icons';
 
@@ -13,10 +13,49 @@ export default () => {
     const [tripsForDisplay, setTripsForDisplay] = useState([]);
     const [viewAllTrips, setViewAllTrips] = useState(true);
     const [filterText, setFilterText] = useState("");
+    const [showDetailModal, setShowDetailModal] = useState(false);
+    const [detailIndex, setDetailIndex] = useState(-1);
 
     useEffect(() => {
         fetchData();
     }, []);
+
+    function closeModal() {
+        setShowDetailModal(false);
+        setDetailIndex(-1);
+    }
+
+    function detailModal() {
+        if (detailIndex == -1) return <></>;
+        const trip = tripsForDisplay[detailIndex];
+        return (
+            <Modal title="Trip details" visible={showDetailModal} onOk={closeModal} onCancel={closeModal} width={'70vw'}>
+                <p>Name: <b>{trip.tripName}</b></p>
+                <p>Destination: <b>{trip.destination}</b></p>
+                <p>Country: <b>{trip.countryName}</b></p>
+                <p>Trip date: <b>{moment(trip.tripDate).format("DD.MM.yyyy")}</b></p>
+
+                <List
+                    itemLayout="horizontal"
+                    dataSource={trip.userList}
+                    renderItem={member => (
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={<Avatar src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png" />}
+                                title={member.fullName}
+                                description={member.email}
+                            />
+                        </List.Item>
+                    )}
+                />
+            </Modal>
+        )
+    }
+
+    function openDetailmodal(idx) {
+        setDetailIndex(idx);
+        setShowDetailModal(true);
+    }
 
     async function fetchData() {
         const trips = await loadTrips();
@@ -54,10 +93,10 @@ export default () => {
             <List
                 itemLayout="horizontal"
                 dataSource={tripsForDisplay}
-                renderItem={trip => (
+                renderItem={(trip, idx) => (
                     <List.Item
                         actions={[<Tooltip title="View detail">
-                            <Button type="primary" shape="circle" icon={<ExpandAltOutlined />} size="large" />
+                            <Button type="primary" shape="circle" onClick={() => openDetailmodal(idx)} icon={<ExpandAltOutlined />} size="large" />
                         </Tooltip>]}
                     >
                         <List.Item.Meta
@@ -68,6 +107,7 @@ export default () => {
                     </List.Item>
                 )}
             />
+            {detailModal()}
         </div>
     )
 }
